4a49eb5c37d705c45dce79da62ec585553de5745
Vladimir Orlov <vladimir.orlov.dev@gmail.com>
2025-11-05 23:26:37 -0300

    Fix tables

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -2,11 +2,13 @@ CREATE TABLE users (
     id         INT UNSIGNED    NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(100)    NOT NULL,
     last_name  VARCHAR(100)    NOT NULL,
+    email      VARCHAR(255)    NOT NULL,
     phone      VARCHAR(32)     NOT NULL,
     password   VARCHAR(50)     NOT NULL,
     created_at INT UNSIGNED    NOT NULL DEFAULT UNIX_TIMESTAMP(),
     updated_at INT UNSIGNED    NOT NULL DEFAULT UNIX_TIMESTAMP(),
 
     PRIMARY KEY (`id`),
-    UNIQUE KEY `users_phone_idx` (`phone`)
+    UNIQUE KEY `users_phone_idx` (`phone`),
+    UNIQUE KEY `users_email_idx` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
